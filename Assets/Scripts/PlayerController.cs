184dd019dd2e3642e432c7f24a6d85003045ebd8
Isa <80214167+ayyyeisa@users.noreply.github.com>
2023-11-10 09:52:17 -0600

    started

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,81 @@
+/*****************************************************************************
+// File Name : PlayerController.cs
+// Author : Isa Luluquisin
+// Creation Date : November 10, 2023
+//
+// Brief Description : This has all the player functions like movement.
+*****************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Variables
+    [SerializeField] private PlayerInput playerInput;
+    [SerializeField] private InputAction move;
+    [SerializeField] private InputAction shoot;
+    [SerializeField] private InputAction restart;
+    [SerializeField] private InputAction quit;
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnableInput();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region Input Actions
+    /// <summary>
+    /// This enables the current action map so that player input is taken in by the keyboard.
+    /// </summary>
+    private void EnableInput()
+    {
+        playerInput.currentActionMap.Enable();
+
+        move = playerInput.currentActionMap.FindAction("Movement");
+        shoot = playerInput.currentActionMap.FindAction("Shoot");
+        restart = playerInput.currentActionMap.FindAction("Restart");
+        quit = playerInput.currentActionMap.FindAction("Quit");
+
+        move.started += Move_Started;
+        move.canceled += Move_Canceled;
+        shoot.started += Shoot_Started;
+        restart.started += Restart_Started;
+        quit.started += Quit_Started;
+
+    }
+     private void Move_Started(InputAction.CallbackContext obj)
+    {
+
+    }
+
+    private void Move_Canceled(InputAction.CallbackContext obj)
+    {
+
+    }
+
+    private void Shoot_Started(InputAction.CallbackContext obj)
+    {
+
+    }
+
+    #endregion
+
+    public void OnDestroy()
+    {
+        move.started -= Move_Started;
+        move.canceled -= Move_Cancelled;
+        shoot.started -= Shoot_Started;
+        restart.started -= Restart_Started;
+        quit.started -= Quit_Started;
+    }
+}
