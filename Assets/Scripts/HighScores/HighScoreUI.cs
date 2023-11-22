4300686a0bf88b42392ca8040d092f9319385b68
Isa <80214167+ayyyeisa@users.noreply.github.com>
2023-11-21 23:02:03 -0600

    before connecting ui to code

--- Assets/Scripts/HighScores/HighScoreUI.cs
+++ Assets/Scripts/HighScores/HighScoreUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HighScoreUI : MonoBehaviour
 {
@@ -8,4 +9,27 @@ public class HighScoreUI : MonoBehaviour
     [SerializeField] private Transform elementWrapper;
 
     List<GameObject> uiElements = new List<GameObject>();
+
+    private void UpdateUI (List<HighScoreElement> list)
+    {
+        for(int i = 0; i < list.Count; i++)
+        {
+            HighScoreElement element = list[i];
+
+            if(element.Points > 0)
+            {
+                if(i >= uiElements.Count)
+                {
+                    var inst = Instantiate(highScoreUIElementPrefab, Vector3.zero, Quaternion.identity);
+                    inst.transform.SetParent(elementWrapper, false);
+
+                        uiElements.Add(inst);
+                }
+
+                //write or overwrite points
+                var texts = uiElements[i].GetComponentsInChildren<Text>();
+                texts[0].text = element.Points.ToString();
+            }
+        }
+    }
 }
